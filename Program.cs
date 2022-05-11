c87405fdf85c1e1243243d9e3b2c568fd972925d
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-11 12:40:32 +0530

    [Zeeshan] : Added homepage for navigating the programs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NunitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t\t Programs for N-Unit Testing");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
