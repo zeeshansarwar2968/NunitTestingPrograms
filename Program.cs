7c48ccb0040f668a1b46445e49657c668049ed52
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-11 13:52:54 +0530

    [Zeeshan] : Added a program to convert the temperature between degC/degF

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace NunitTesting
             Console.WriteLine("");
             Console.WriteLine("1. Find the day of the week");
             Console.WriteLine("");
+            Console.WriteLine("2. Convert the temperature to either Fahrenheit/Celcius");
+            Console.WriteLine("");
             Console.WriteLine("\t\t\t\t Press 0 to exit the program");
             Console.WriteLine("");
 
@@ -26,6 +28,9 @@ namespace NunitTesting
                         break;
                     case 1:
                         DayofWeek.CalculateDay();
+                        break;
+                    case 2:
+                        TemperatureConversion.CalculateTemperature();
                         break;
                     default:
                         Console.WriteLine("Please Enter from the given choice");
---
+++ TemperatureConversion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting
+{
+    internal class TemperatureConversion
+    {
+        internal static void CalculateTemperature()
+        {
+            //Taking input from the user for calculation
+            Console.WriteLine("Available Conversion Choices");
+            Console.WriteLine("1. Celsius to Fahrenheit");
+            Console.WriteLine("2. Fahrenheit to Celsius");
+            Console.Write("Please Choose Conversion Option : ");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            if (option == 1)
+            {
+                Console.Write("Enter temperature in degC : ");
+                double celsius = Convert.ToDouble(Console.ReadLine());
+
+                //Formula to convert deg Celsius to Fahrenheit
+                double fahrenheit = (celsius * 9 / 5) + 32;
+                Console.WriteLine("Temperature in degree Fahrenheit is : " + fahrenheit + " degree Fahrenheit");
+            }
+            else if (option == 2)
+            {
+                Console.Write("Enter temperature in degF : ");
+                double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+                //Formula to convert deg Fahrenheit to Celsius
+                double celsius = (fahrenheit - 32) * 5 / 9;
+                Console.WriteLine("Temperature in degree Celsius is : " + celsius + " degree Celsius");
+            }
+
+            else
+            {
+                Console.Write("Invalid Selection. Please Choose from the available options");
+
+            }
+        }
+    }
+}
